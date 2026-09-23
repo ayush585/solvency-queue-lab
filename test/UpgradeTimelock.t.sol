4f5d84cd729e6512f66290b239a21ba6449729d7
Ayushman Mukherjee <127019874+ayush585@users.noreply.github.com>
2026-09-23 21:30:29 +0530

    test: preserve admin prank for canceller role grant

--- test/UpgradeTimelock.t.sol
+++ test/UpgradeTimelock.t.sol
@@ -37,8 +37,9 @@ contract UpgradeTimelockTest is Test {
 
         timelock = new TimelockController(DELAY, proposers, executors, admin);
 
+        bytes32 cancellerRole = timelock.CANCELLER_ROLE();
         vm.prank(admin);
-        timelock.grantRole(timelock.CANCELLER_ROLE(), proposer);
+        timelock.grantRole(cancellerRole, proposer);
 
         bytes memory initData =
             abi.encodeCall(UpgradeVaultV1.initialize, (address(timelock), 1_500));
